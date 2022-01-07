9a9c22c2cbec2adcff46aa19c7202f6dccc57e1a
joeneedum <joeneedum@gmail.com>
2022-01-07 02:38:46 -0500

    commit

---
+++ src/Components/Clicker.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Clicker extends Component {
+    constructor(props){
+        super(props);
+        this.state = {num: 1}
+        this.genRandom = this.genRandom.bind(this)
+    }
+    genRandom() {
+        let rand = Math.floor(Math.random() * 10) + 1;
+
+        this.setState({ num: rand })
+    }
+    // genRandom = (e) => {
+
+    //     let rand = Math.floor(Math.random() * 10) + 1;
+
+    //     this.setState({ num: rand })
+    // }
+    render() {
+        return (
+            <div className='App'>
+                <h1>Number is {this.state.num}</h1>
+                {this.state.num === 7
+                ? <h2>YOU WIN!</h2>
+                : <button onClick={this.genRandom}>Random Number</button>
+                }
+            </div>
+        )
+    }
+}
